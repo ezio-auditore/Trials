d2073f8c71717eca470267322efb89b3b0977fd7
unknown <kaustavmajumder93@yahoo.com>
2015-10-17 12:56:53 +0530

    Start of my new project

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>just practice</title>
+	<link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+<div class="container">
+<div class="nav nav-bar nav">
+	<ul>
+		<li>Home</li>
+		<li>My Favourites</li>
+		<li>About me</li>
+	</ul>
+</div>
+</div>
+
+</body>
+</html>
